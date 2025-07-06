1c4e5777253b07d72c89403dfa3350d8e59f60e3
ANaNd@07 <ap123vapi@gmail.com>
2025-07-06 21:23:06 +0530

    updated socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -12,7 +12,7 @@ const getRoomId = (userId, targetId) => {
 const initializeSocket = (server) => {
     const io = socket(server, {
         cors: {
-            origin: "http://localhost:5173",
+            origin: "https://chat-app-frontend-q91gik3ho-anands-projects-6d91dd8d.vercel.app",
         }
     })
     
